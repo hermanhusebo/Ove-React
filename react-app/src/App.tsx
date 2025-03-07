d8823298ab2697625bfc7c820559df76e2b213c2
Herman Husebo <hermanhusebo@gmail.com>
2025-03-07 18:12:59 +0100

    Oppdatering

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,10 @@
 import ListGroup from "./components/ListCroup";
 
 function App(){
-  return <div><ListGroup/></div>
+  let items = ["New York", "London"];
+  const handleSelectItem = (item:string) =>{
+    console.log(item)
+  }
+  return <div><ListGroup items = {items} heading = "Cities" onSelectItem={handleSelectItem}/></div>
 }
 export default App;
