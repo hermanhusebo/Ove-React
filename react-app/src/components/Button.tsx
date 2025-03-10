4fe54ce0545a49aa8edf64379bcfac5af41b81e2
Herman Husebo <hermanhusebo@gmail.com>
2025-03-10 11:58:39 +0100

    Initianize button

---
+++ react-app/src/components/Button.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+interface Props {
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+//Alt her er foreløpig bare kopiert fra ListGroup
+
+function Button({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
